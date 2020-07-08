02c6fd8ffd4a6b0db1699e321a2f9bf9f0b89da4
zero09285 <wuchyng1111@gmail.com>
2020-07-08 18:57:19 +0800

    0708

---
+++ js/loading.js
@@ -0,0 +1,9 @@
+window.onload = function () { //Loading效果 與 Aos初始化
+
+    setTimeout(function () {
+        $(".loading_page").fadeOut(500);
+    }, 500);
+
+    $(".loading_page").fadeIn(200)
+
+};
